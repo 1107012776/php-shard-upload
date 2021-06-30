b71715992ed1694dca4e92e9fa29cd1a7b629ecd
linyushan <linyushan@nawang.cn>
2021-06-30 08:41:05 +0800

    备注修改

--- src/ShardUpload.php
+++ src/ShardUpload.php
@@ -1,5 +1,12 @@
 <?php
-
+/**
+ * PhpShardUpload  file.
+ * @author linyushan  <1107012776@qq.com>
+ * @link https://www.developzhe.com/
+ * @package https://github.com/1107012776/php-shard-upload
+ * @copyright Copyright &copy; 2019-2021
+ * @license https://github.com/1107012776/php-shard-upload/blob/master/LICENSE
+ */
 namespace PhpShardUpload;
 
 use PhpShardUpload\Components\ServiceTrait;
