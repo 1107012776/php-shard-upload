3d5c223781a0d33053cbe61f08d2c0aa5ca64f95
linyushan <linyushan@nawang.cn>
2021-06-30 10:32:05 +0800

    获取已成功上传的文件

---
+++ src/FileManage.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * PhpShardUpload  file.
+ * @author linyushan  <1107012776@qq.com>
+ * @link https://www.developzhe.com/
+ * @package https://github.com/1107012776/php-shard-upload
+ * @copyright Copyright &copy; 2019-2021
+ * @license https://github.com/1107012776/php-shard-upload/blob/master/LICENSE
+ */
+namespace PhpShardUpload;
+
+use PhpShardUpload\Components\ServiceTrait;
+class FileManage{
+    use ServiceTrait;
+    protected $md5Hash; //要上传的文件的md5Hash
+    protected $sha1Hash;  //要上传的文件的sha1Hash
+    protected $fileBaseDir;  //基础文件存储根目录
+    public function __construct($md5Hash, $sha1Hash, $fileBaseDir)
+    {
+        $this->md5Hash = $md5Hash;
+        $this->sha1Hash = $sha1Hash;
+        $this->fileBaseDir = $fileBaseDir;
+    }
+
+    /**
+     * 文件路径
+     * @return string
+     */
+    public function getUploadSuccessFilePath(){
+        return $this->fileBaseDir . $this->md5Hash . '_' . $this->sha1Hash.'.data';
+    }
+}
--- tests/fileStatus.php
+++ tests/fileStatus.php
@@ -20,5 +20,9 @@ $sha1Hash = $_POST['sha1Hash'];
 $fileBaseDir = './fileDir/';
 $shard = new ShardUploadStatus($total, $shardSize, $size, $md5Hash, $sha1Hash, $fileBaseDir);
 $response = $shard->getUploadStatus();
+if($response['status'] == 1){
+   $manage = new \PhpShardUpload\FileManage($md5Hash, $sha1Hash, $fileBaseDir);
+   var_dump($manage->getUploadSuccessFilePath()); //已成功上传的文件路径
+}
 header('Content-Type:application/json;charset=utf-8');
 echo json_encode($response,JSON_UNESCAPED_UNICODE);
